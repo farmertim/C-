814e2e294b883ed6c19777be263d8baa09dda174
farmertim <52063135+farmertim@users.noreply.github.com>
2019-12-23 15:40:20 +0800

    Practice

---
+++ 20191223/資料繫結.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="資料繫結.aspx.cs" Inherits="資料繫結" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    <div>
+    
+        <asp:DropDownList ID="DropDownList1" runat="server">
+        </asp:DropDownList>
+        <br />
+        <asp:CheckBoxList ID="CheckBoxList1" runat="server" RepeatDirection="Horizontal">
+        </asp:CheckBoxList>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20191223/資料繫結.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+public partial class 資料繫結 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        DataTable dt = getName();
+        DropDownList1.DataSource = dt;
+        DropDownList1.DataTextField = dt.Columns["姓名"].ToString();
+        DropDownList1.DataBind();
+
+        CheckBoxList1.DataSource = dt;
+        CheckBoxList1.DataTextField = dt.Columns["帳號"].ToString();
+        CheckBoxList1.DataBind();
+    }
+    protected DataTable getName() {
+        using (SqlConnection co = new SqlConnection("Data Source=LAPTOP-Q9A6IMGN\\SQLEXPRESS;Initial Catalog=運動與飲食紀錄;Integrated Security=True")) {
+
+            SqlDataAdapter da = new SqlDataAdapter("Select * from 客戶資料",co);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return dt;
+        }
+        
+    }
+}
