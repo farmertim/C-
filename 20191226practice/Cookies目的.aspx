0d59d6f515249f038281ee95e01838d2be01a55a
farmertim <52063135+farmertim@users.noreply.github.com>
2019-12-27 13:12:08 +0800

    practice

---
+++ 20191226practice/Cookies目的.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Cookies目的.aspx.cs" Inherits="Cookies目的" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Name:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        Grade:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <br />
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="登出" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20191226practice/Cookies目的.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Cookies目的 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+           
+            TextBox2.Text = Session["name"].ToString();
+            if (Request.Cookies[Session["name"].ToString()] != null)
+            {
+                Response.Write("<script>alert('" + Request.Cookies[Session["name"].ToString()].Value + "');</script>");
+                TextBox1.Text = Request.Cookies[Session["name"].ToString()]["Grade"];
+            }
+            TextBox2.Enabled = false;
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Cookies[Session["name"].ToString()].Value = TextBox2.Text;
+
+        if (Request.Cookies[TextBox2.Text] != null)
+        {
+           
+            Response.Cookies[TextBox2.Text]["Grade"] = TextBox1.Text;
+        }
+        }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Session.Abandon();
+        Response.Redirect("cookies.aspx");
+    }
+}
