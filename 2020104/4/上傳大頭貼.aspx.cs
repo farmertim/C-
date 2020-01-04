97a1da29bf4f929ec6563b4c92606eb7cea27cb1
farmertim <52063135+farmertim@users.noreply.github.com>
2020-01-04 22:53:33 +0800

    practice

--- 2020104/4/上傳大頭貼.aspx.cs
+++ 2020104/4/上傳大頭貼.aspx.cs
@@ -11,47 +11,82 @@ public partial class 上傳大頭貼 : System.Web.UI.Page
     string s;
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (IsPostBack) {
-               Image1.ImageUrl = "~/" + s;
+        string web = Request.PhysicalApplicationPath + "\\";
+        using (StreamReader sr = new StreamReader(web + "\\abc.txt"))
+        {
+            s = sr.ReadLine();
+            Response.Write("<script>alert('" + s + "')</script>");
+
+            Image1.ImageUrl = "~/" + s;
+            sr.Close();
+            sr.Dispose();
         }
     }
 
     protected void Button1_Click(object sender, EventArgs e)
     {
-       
-        string web = Request.PhysicalApplicationPath+"\\";
+        //string web = Request.PhysicalApplicationPath+"//";
+        //string file = FileUpload1.FileName;
+        //FileUpload1.SaveAs(web + file);
+        //if (File.Exists(web + "\\下載.txt"))
+        //{
+        //    FileStream fs = new FileStream(web + "\\下載.txt", FileMode.Append);
+        //    StreamWriter sw = new StreamWriter(fs);
+        //    sw.Write(FileUpload1.FileName);
+        //    sw.Flush();
+        //} else {
+        //    StreamWriter ff = File.CreateText(web + "\\下載.txt");
+        //    ff.Write(FileUpload1.FileName);
+        //    ff.Flush();
+        //}
+
+
+
+
+
+
+        string web = Request.PhysicalApplicationPath + "\\";
         string file = FileUpload1.FileName;
         FileUpload1.SaveAs(web + file);
-        if (File.Exists(web + "\\abc.txt")) {
+        if (File.Exists(web + "\\abc.txt"))
+        {
             Response.Write("file 已存在");
 
-            FileStream fs = new FileStream(web + "\\abc.txt", FileMode.Append);
-            StreamWriter sw = new StreamWriter(fs);
-            sw.WriteLine("abcdefg");
-            sw.Write(FileUpload1.FileName);
+            //FileStream fs = new FileStream(web + "\\abc.txt", FileMode.Append);
+            using (StreamWriter sw = new StreamWriter(web + "\\abc.txt"))
+            {
+                //sw.WriteLine("abcdefg");
+                Response.Write("<script>alert('" + FileUpload1.FileName + "')</script>");
+                sw.Write(FileUpload1.FileName);
 
-            sw.Flush();
-            fs.Close();
-            fs.Dispose();
+                sw.Flush();
+                sw.Close();
+            }
+            //fs.Close();
+           // fs.Dispose();
         }
-        else {
+        else
+        {
             StreamWriter sw = File.CreateText(web + "\\abc.txt");
             sw.Write(FileUpload1.FileName);
             sw.Flush();
         }
+
         /*
         using (StreamWriter sw = File.AppendText(web + "\\下載.txt")) {
             sw.Write(FileUpload1.FileName);
             sw.Flush();
         }
         */
-        /*
-        using (StreamReader sr = new StreamReader(web + "\\下載.txt")) {
+        
+        using (StreamReader sr = new StreamReader(web + "\\abc.txt")) {
             s = sr.ReadLine();
-            Response.Write(s);
+            Response.Write("<script>alert('" + s + "')</script>");
+
+            Image1.ImageUrl = "~/"+ s;
             sr.Close();
             sr.Dispose();
         }
-        */
+        
     }
 }
